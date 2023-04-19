d473b16124a435bf039480ac6599942273e2aa82
natalija-novakovic <natalijadsgn@gmail.com>
2023-04-19 18:55:03 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {nanoid} from 'nanoid';
+import NotesList from './components/NotesList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note',
+      date: '15/04/2021'
+    }, 
+    {
+      id: nanoid(),
+      text: 'This is my second note',
+      date: '15/04/2021'
+    }, 
+    {
+      id: nanoid(),
+      text: 'This is my third note',
+      date: '15/04/2021'
+    }, 
+    {
+      id: nanoid(),
+      text: 'This is my fourth note',
+      date: '15/04/2021'
+    }, 
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+
+  };
+
+  return ( 
+  <div className="container">
+    <NotesList notes={notes} handleAddNote={addNote} />
+  </div> );
 }
-
+ 
 export default App;
